616c2e2e40d96bb34571ed9b717f3451214f71f9
ericzong <ericzonglu@126.com>
2018-04-15 23:51:51 +0800

    Tree Traverse

--- src/main/java/com/ericzong/algorithm/binarytree/TreeNode.java
+++ src/main/java/com/ericzong/algorithm/binarytree/TreeNode.java
@@ -1,5 +1,8 @@
 package com.ericzong.algorithm.binarytree;
 
+import java.util.LinkedList;
+import java.util.function.Consumer;
+
 public class TreeNode {
     private TreeNode left;
     private TreeNode right;
@@ -20,6 +23,94 @@ public class TreeNode {
         this.left = left;
         this.right = right;
         this.data = data;
+    }
+
+    public void traversePreOrder(Consumer<TreeNode> op) {
+        op.accept(this);
+        if(this.getLeft() != null) {
+            this.getLeft().traversePreOrder(op);
+        }
+        if(this.getRight() != null) {
+            this.getRight().traversePreOrder(op);
+        }
+    }
+
+    public void traversePreOrderLoop(Consumer<TreeNode> op) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode top = this;
+        stack.push(null);
+
+        while (top != null) {
+            op.accept(top);
+            if(top.getRight() != null) {
+                stack.push(top.getRight());
+            }
+            if(top.getLeft() != null) {
+                stack.push(top.getLeft());
+            }
+
+            top = stack.pop();
+        }
+    }
+
+    public void traverseInOrder(Consumer<TreeNode> op) {
+        if(this.getLeft() != null) {
+            this.getLeft().traverseInOrder(op);
+        }
+        op.accept(this);
+        if(this.getRight() != null) {
+            this.getRight().traverseInOrder(op);
+        }
+    }
+
+    public void traverseInOrderLoop(Consumer<TreeNode> op) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode top = this;
+
+        while(!stack.isEmpty() || top != null) {
+            if(top != null) {
+                stack.push(top);
+                top = top.getLeft();
+            } else {
+                // 左孩子节点为空返回
+                top = stack.pop();
+                op.accept(top);
+                top = top.getRight();
+            }
+        }
+    }
+
+    public void traversePostOrder(Consumer<TreeNode> op) {
+        if(this.getLeft() != null) {
+            this.getLeft().traversePostOrder(op);
+        }
+        if(this.getRight() != null) {
+            this.getRight().traversePostOrder(op);
+        }
+        op.accept(this);
+    }
+
+    public void traversePostOrderLoop(Consumer<TreeNode> op) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode top = this;
+        TreeNode peek;
+        TreeNode lastVisited = null;
+
+        while(!stack.isEmpty() || top != null) {
+            if(top != null) {
+                stack.push(top);
+                top = top.getLeft();
+            } else {
+                peek = stack.peek();
+                if(peek.getRight() != null && lastVisited != peek.getRight()) {
+                    top = peek.getRight();
+                } else {
+                    stack.pop();
+                    op.accept(peek);
+                    lastVisited = peek;
+                }
+            }
+        }
     }
 
     public boolean isBalanced() {
